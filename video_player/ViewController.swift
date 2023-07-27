c8b4cac92a6b8cdc953c00eefaabf7155bb7a9ce
daniel <nwogwugwudaniel@gmail.com>
2023-07-27 01:15:44 +0100

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by DANIEL on 27/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
