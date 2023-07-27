3e8cffa455463ded5dd61374173efdcf8f0c6536
daniel <nwogwugwudaniel@gmail.com>
2023-07-27 23:26:48 +0100

    Setup basic video player with  the Avplayer class

--- video_player/ViewController.swift
+++ video_player/ViewController.swift
@@ -6,12 +6,31 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playVideo()
+    }
+    
+    private func playVideo(){
+        guard let path = Bundle.main.path(forResource: "THE+WEDDING+WEEKEND+(LATEST+MOVIE)+__+MOUNT+ZION+__+FLAMING+SWORD+latest+movie", ofType: "mp4") else{
+            debugPrint("video not found")
+            return
+        }
+        let player = AVPlayer(url:URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true)
+//        player.play()
     }
 
 
